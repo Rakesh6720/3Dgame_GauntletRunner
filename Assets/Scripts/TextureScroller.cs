e37c59b849861b9243664a52633e8ed8ecd60f3c
Rakesh Baruah <rbaruah@gmail.com>
2018-06-30 12:22:38 -0500

    second commit.  have setup scene.  next step is to begin animating the player character.

--- Assets/Scripts/TextureScroller.cs
+++ Assets/Scripts/TextureScroller.cs
@@ -4,13 +4,24 @@ using UnityEngine;
 
 public class TextureScroller : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public float speed = .5f;
+
+    Renderer renderer;
+    float offset;
+
+    private void Start()
+    {
+        renderer = GetComponent<Renderer>();
+    }
+
+    private void Update()
+    {
+        //increase the offset based on time
+        offset += Time.deltaTime * speed;
+        //keep offset between 0 and 1
+        if (offset > 1)
+            offset -= 1;
+        //apply the offset to the material
+        renderer.material.mainTextureOffset = new Vector2(0, offset);
+    }
 }
